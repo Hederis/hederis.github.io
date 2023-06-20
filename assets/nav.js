41cea76eebd8dddd2ff2789256f2c96ad4f82b3d
Nellie McKesson <nelliemckesson@gmail.com>
2023-06-20 13:16:12 -0700

    adjusting mobile nav

--- assets/nav.js
+++ assets/nav.js
@@ -1,12 +1,35 @@
 /* nav */
 
-$(function() {
+window.addEventListener('load', function() {
 
-	console.log("loaded!");
+	console.log('loaded');
 
-	$('.mobile_nav_btn').on('click', function() {
-		console.log('click');
-		$('div.nav').toggleClass('active');
-	})
+	function toggleClassJS(element, myclass) {
+	  var classes = element.className;
+	  if (classes) {
+	    classes = classes.split(" ");
+	  } else {
+	    classes = [];
+	  }
+	  var i = classes.indexOf(myclass);
+	  if (i < 0) {
+	    classes.push(myclass);
+	  } else {
+	  	classes.splice(i, 1);
+	  }
+	  element.className = classes.join(" ");
+	}
+
+	var mobileNav = document.querySelector('#home_nav .mobile_nav_btn, #pricing_nav .mobile_nav_btn');
+
+	console.log(mobileNav);
+
+	mobileNav.addEventListener("click", (event) => {
+	 	console.log('click');
+		var nav = document.querySelector("div.nav");
+		toggleClassJS(nav, "active");
+		var navBtn = document.querySelector(".mobile_nav_btn");
+		toggleClassJS(navBtn, "active");
+	});
 
 })
